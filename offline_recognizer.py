92f7a14112d56659ad5b9a42887d53e058d962f7
hat359 <48184672+hat359@users.noreply.github.com>
2023-02-21 15:41:00 -0500

    Part d - logfile

--- offline_recognizer.py
+++ offline_recognizer.py
@@ -3,6 +3,7 @@ from recognizer import Recognizer
 from copy import deepcopy
 from random import randint
 from json import dumps
+import csv
 
 class OfflineRecognizer():
     def __init__(self):
@@ -25,11 +26,12 @@ class OfflineRecognizer():
     
     def recognizeOfflineData(self):
         score = {}
+        logcsv = []
         for user in self.preProcessedData: # For each user
             score[user] = {}
-            for example in range(1,10): # For each example from 1 to 9
+            for example in range(1,3): # For each example from 1 to 9
                 score[user][example] = {}
-                for i in range(1,11): # For iterations from 1 to 10
+                for i in range(1,2): # For iterations from 1 to 10
                     # print(len(self.preProcessedData[user]['medium']))
                     # print(len(self.preProcessedData[user]['medium']['arrow']))
                     # print(len(self.preProcessedData[user]['medium']['arrow'][0]))
@@ -37,14 +39,21 @@ class OfflineRecognizer():
                     # print(len(training_set))
                     # print(len(testing_set))
                     recognizer = Recognizer(training_set)
+
                     for gesture,points in testing_set.items():
+                        # print(gesture)
                         if gesture not in score[user][example]:
                             score[user][example][gesture] = 0
-                        recognizedGesture, _, _ = recognizer.recognizeGesture(points)
+                        recognizedGesture, _, _,Nbest = recognizer.recognizeGesture(points)
+                        # print(recognizedGesture)
+                        log = {'User':user,'Gesture':gesture,'Iteration':i,'Example':example,'TrainingSize':len(training_set),'TrainingContents':training_set,'RecognizedGesture':recognizedGesture,'Nbest':Nbest}
+                        
+                        logcsv.append(log)
                         recognizedGesture = recognizedGesture.split('/')[0] if recognizedGesture!=None else ''
                         if recognizedGesture == gesture:
                             score[user][example][gesture] += 1
         self.writeToFile(dumps(score), 'score.json')
+        self.writeToCsv(logcsv,'logfile.csv')
     
 
     def getSplitData(self, gestures, E):
@@ -62,3 +71,14 @@ class OfflineRecognizer():
         file.close()
                 
 
+    def writeToCsv(self,dict_data,filename):
+        csv_columns = ['User','Gesture','Iteration','Example','TrainingSize','TrainingContents','RecognizedGesture','Nbest']
+        csv_file=filename
+        try:
+            with open(csv_file, 'w') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+                writer.writeheader()
+                for data in dict_data:
+                    writer.writerow(data)
+        except IOError:
+            print("I/O error")
